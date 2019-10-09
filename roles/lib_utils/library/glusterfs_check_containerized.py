504041d986325b6ffb1405d09f7e4e744cc96a25
Daniel-Pivonka <dpivonka@redhat.com>
2019-10-09 10:51:09 -0400

    d

--- roles/lib_utils/library/glusterfs_check_containerized.py
+++ roles/lib_utils/library/glusterfs_check_containerized.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import subprocess
 import datetime
 from ansible.module_utils.basic import AnsibleModule
 
